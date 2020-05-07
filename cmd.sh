e83bd60b75ffa0cd60fb2530706d64b63247c93a
Alessandro Pasotti <elpaso@itopen.it>
2020-05-07 15:10:23 +0200

    Use Xvfb, and a cmd.sh script as CMD

---
+++ cmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# CMD to be run inside the g3w-suite docker, this script is
+# volume-mounted (ro) in the docker compose as /cmd.sh
+
+# Start XVfb
+Xvfb :99 -screen 0 640x480x24 -nolisten tcp &
+export DISPLAY=:99
+# Build the suite
+/code/ci_scripts/build_suite.sh
+# Setup once
+/code/ci_scripts/setup_suite.sh
+# Start
+cd /code/g3w-admin
+gunicorn base.wsgi:application --limit-request-fields 0 --error-logfile - \
+    --log-level=debug --timeout 120 --workers=${G3WSUITE_GUNICORN_NUM_WORKERS:-8} -b 0.0.0.0:8000
