be444f3b08708c15c614794486c5a86268eae8d7
Alessandro Pasotti <elpaso@itopen.it>
2021-03-30 15:29:50 +0200

    Add openrouteservice

---
+++ scripts/docker-consumer-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Starts huey consumer
+
+cd /code/g3w-admin
+
+# Wait for the main suite to start
+wait-for-it -h g3w-suite -p 8000 -t 60
+
+export QGIS_SERVER_PARALLEL_RENDERING=1
+
+cd /code/g3w-admin
+
+# Start the consumer
+/usr/bin/xvfb-run -a python3 manage.py run_huey
