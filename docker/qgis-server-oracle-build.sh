9be703c008a5fa0fe0f5bc818a1496ae6148769d
wlorenzetti <lorenzett@gis3w.it>
2022-05-23 10:04:47 +0200

    Update dockerfiles for oracle.

--- docker/qgis-server-oracle-build.sh
+++ docker/qgis-server-oracle-build.sh
@@ -1,11 +1,11 @@
 # Build the base QGIS Server with Oracle FCGI image
 #
 # Arguments:
-#  DOCKER_DEPS_TAG: tag for the dependencies base image, ex: release-3_10
-#  QGIS_TAG: git tag from the QGIS repo, ex: final-3_10_12
+#  DOCKER_DEPS_TAG: tag for the dependencies base image, ex: release-3_22
+#  QGIS_TAG: git tag from the QGIS repo, ex: final-3_22_7
 
-QGIS_TAG=${QGIS_TAG:-"final-3_10_12"}
-DOCKER_DEPS_TAG=${DOCKER_DEPS_TAG:-"release-3_10"}
+QGIS_TAG=${QGIS_TAG:-"final-3_22_7"}
+DOCKER_DEPS_TAG=${DOCKER_DEPS_TAG:-"release-3_22"}
 
 docker build \
     -f Dockerfile.qgis-server-oracle.dockerfile \
