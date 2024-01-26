57a87411674ade32964f33bfe1e1dae2fa115150
wlorenzetti <lorenzett@gis3w.it>
2024-01-26 10:53:36 +0100

    Clone g3w-admin code with depth 1.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 
 if [ -z "$(ls -A /code)" ]; then
    echo "Cloning g3w-admin branch ${G3W_SUITE_BRANCH:-dev} ..."
-   git clone https://github.com/g3w-suite/g3w-admin.git --single-branch --branch ${G3W_SUITE_BRANCH:-dev} /code && \
+   git clone --depth=1 https://github.com/g3w-suite/g3w-admin.git --single-branch --branch ${G3W_SUITE_BRANCH:-dev} /code && \
    cd /code
 fi
 
