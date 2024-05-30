1e441502999e6361541fcf2ad5049d97d305098a
wlorenzetti <lorenzett@gis3w.it>
2024-05-30 11:00:19 +0200

    Add update postgis command.

--- upgrade_postgres_11_to_16_RESTORE.sh
+++ upgrade_postgres_11_to_16_RESTORE.sh
@@ -9,12 +9,15 @@ rm .pgpass
 
 # Create a script fro backup
 echo "psql --host ${G3WSUITE_POSTGRES_HOST}  --port ${G3WSUITE_POSTGRES_PORT} --username ${G3WSUITE_POSTGRES_USER_LOCAL} -d g3wsuite -f /var/lib/postgresql/11/g3wsuite.sql" > pg_restore.sh
+echo "psql --host ${G3WSUITE_POSTGRES_HOST}  --port ${G3WSUITE_POSTGRES_PORT} --username ${G3WSUITE_POSTGRES_USER_LOCAL} -d g3wsuite -c \"select postgis_extensions_upgrade();\"" >> pg_restore.sh
 echo "psql --host ${G3WSUITE_POSTGRES_HOST}  --port ${G3WSUITE_POSTGRES_PORT} --username ${G3WSUITE_POSTGRES_USER_LOCAL} -d data_production -f /var/lib/postgresql/11/data_production.sql" >> pg_restore.sh
+echo "psql --host ${G3WSUITE_POSTGRES_HOST}  --port ${G3WSUITE_POSTGRES_PORT} --username ${G3WSUITE_POSTGRES_USER_LOCAL} -d data_production -c \"select postgis_extensions_upgrade();\"" >> pg_restore.sh
 echo "psql --host ${G3WSUITE_POSTGRES_HOST}  --port ${G3WSUITE_POSTGRES_PORT} --username ${G3WSUITE_POSTGRES_USER_LOCAL} -d data_testing -f /var/lib/postgresql/11/data_testing.sql" >> pg_restore.sh
+echo "psql --host ${G3WSUITE_POSTGRES_HOST}  --port ${G3WSUITE_POSTGRES_PORT} --username ${G3WSUITE_POSTGRES_USER_LOCAL} -d data_testing -c \"select postgis_extensions_upgrade();\"" >> pg_restore.sh
 
 docker compose cp pg_restore.sh postgis:/root/
 docker compose exec postgis chmod +x /root/pg_restore.sh
-rm pg_restore.sh
+#rm pg_restore.sh
 
 docker compose exec postgis bash /root/pg_restore.sh
 docker compose restart g3w-suite
