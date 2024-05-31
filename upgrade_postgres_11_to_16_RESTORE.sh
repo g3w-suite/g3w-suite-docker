42c50c605f411fef4550008acf5d647b0d6d6846
wlorenzetti <lorenzett@gis3w.it>
2024-05-31 15:09:12 +0200

    Use of template1 as db for connection.

--- upgrade_postgres_11_to_16_RESTORE.sh
+++ upgrade_postgres_11_to_16_RESTORE.sh
@@ -5,22 +5,22 @@ source update_postgres_11_to_16_BASE.sh
 echo "#!/bin/bash" > pg_restore.sh
 for DB in "${DBS[@]}"; do
 
-  echo "psql $CONNECTION_DB -d ${DB} -c \"create database ${DB}_${SUFFIX_DB};\"" >> pg_restore.sh
+  echo "psql $CONNECTION_DB -d ${RESTORE_CONNECTION_DBNAME} -c \"create database ${DB}_${SUFFIX_DB};\"" >> pg_restore.sh
   echo "pg_restore $CONNECTION_DB -d ${DB}_${SUFFIX_DB} ${BACKUP_PATH}${DB}.bck" >> pg_restore.sh
   echo "psql ${CONNECTION_DB} -d ${DB}_${SUFFIX_DB} -c \"${QUERY_UPGRADE_POSTGIS}\"" >> pg_restore.sh
 
   # Close connection
-  echo "psql ${CONNECTION_DB} -d ${DB}_${SUFFIX_DB} -c \"${QUERY_CLOSE_CONNECTIONS}'db_name';\"" >> pg_restore.sh
+  echo "psql ${CONNECTION_DB} -d ${RESTORE_CONNECTION_DBNAME} -c \"${QUERY_CLOSE_CONNECTIONS}'${DB}';\"" >> pg_restore.sh
 
   # Drop old database adn rename the new with old
-  echo "psql ${CONNECTION_DB} -d ${DB}_${SUFFIX_DB} -c \"drop database ${DB};\"" >> pg_restore.sh
-  echo "psql ${CONNECTION_DB} -d ${DB}_${SUFFIX_DB} -c \"alter database ${DB}_${SUFFIX_DB} rename to ${DB};\"" >> pg_restore.sh
+  echo "psql ${CONNECTION_DB} -d ${RESTORE_CONNECTION_DBNAME} -c \"drop database ${DB};\"" >> pg_restore.sh
+  echo "psql ${CONNECTION_DB} -d ${RESTORE_CONNECTION_DBNAME} -c \"alter database ${DB}_${SUFFIX_DB} rename to ${DB};\"" >> pg_restore.sh
 
 done
 
 docker compose cp pg_restore.sh postgis:/root/
 docker compose exec postgis chmod +x /root/pg_restore.sh
-#rm pg_restore.sh
+rm pg_restore.sh
 
 docker compose exec postgis bash /root/pg_restore.sh
 docker compose restart g3w-suite
