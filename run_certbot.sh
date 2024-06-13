68fd74e8c0a706aa7169a5dc2365ab1bc5eff4b0
Raruto <Raruto@users.noreply.github.com>
2024-06-13 13:12:10 +0200

    Update `run_certbot.sh`

--- run_certbot.sh
+++ run_certbot.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
-# Run certbot docker container to renew the HTTPS certificate.
-# Requires .env file with container configuration variables
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source ${CURRENT_DIR}/.env
-
-if [ "${WEBGIS_PUBLIC_HOSTNAME}" = "" ]; then
-    echo "WEBGIS_PUBLIC_HOSTNAME not defined: exiting"
-    exit 1
-fi
-
-if [ "${WEBGIS_DOCKER_SHARED_VOLUME}" = "" ]; then
-    echo "WEBGIS_DOCKER_SHARED_VOLUME not defined: exiting"
-    exit 1
-fi
-
-mkdir -p "${WEBGIS_DOCKER_SHARED_VOLUME}/certs/letsencrypt/"
-
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "${WEBGIS_DOCKER_SHARED_VOLUME}/certs/letsencrypt/options-ssl-nginx.conf"
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "${WEBGIS_DOCKER_SHARED_VOLUME}/certs/letsencrypt/ssl-dhparams.pem"
-
-docker run -it --rm --name certbot \
-  -v ${WEBGIS_DOCKER_SHARED_VOLUME}/certs/letsencrypt:/etc/letsencrypt \
-  -v ${WEBGIS_DOCKER_SHARED_VOLUME}/var/www/.well-known:/var/www/.well-known \
-  certbot/certbot -t certonly \
-  --agree-tos --renew-by-default \
-  --no-eff-email \
-  --webroot -w /var/www \
-  -d ${WEBGIS_PUBLIC_HOSTNAME}
+make -C $CURRENT_DIR renew-ssl ENV=DEV
