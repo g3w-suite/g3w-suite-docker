ef93cf10396c00f3be84dc94c62c0ce8e3f80db8
wlorenzetti <lorenzett@gis3w.it>
2024-11-07 14:49:56 +0100

    Fix for Python 3.12

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,8 +10,8 @@ fi
 cp /requirements_rl.txt .
 
 # Override settings
-pip3 install -r requirements_rl.txt
-pip3 install -r requirements_huey.txt
+pip3 install -r requirements_rl.txt --break-system-packages
+pip3 install -r requirements_huey.txt --break-system-packages
 
 # Front end
 #TODO make this as generic so that we can install as many plugins as possible
@@ -19,14 +19,14 @@ git submodule add -f https://github.com/g3w-suite/g3w-admin-frontend.git  g3w-ad
 
 
 # Caching
-pip3 install -r /code/g3w-admin/caching/requirements.txt
+pip3 install -r /code/g3w-admin/caching/requirements.txt --break-system-packages
 
 # File manager
-pip3 install -r /code/g3w-admin/filemanager/requirements.txt
+pip3 install -r /code/g3w-admin/filemanager/requirements.txt --break-system-packages
 
 # Qplotly
-pip3 install -r /code/g3w-admin/qplotly/requirements.txt
+pip3 install -r /code/g3w-admin/qplotly/requirements.txt --break-system-packages
 
 # Openrouteservice
-pip3 install -r /code/g3w-admin/openrouteservice/requirements.txt
+pip3 install -r /code/g3w-admin/openrouteservice/requirements.txt --break-system-packages
 
